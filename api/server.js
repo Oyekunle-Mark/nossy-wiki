0f84b926ba15459ba378554bf86e61b5c8f69b34
oyekunle-mark <oyekunlemac@gmail.com>
2019-08-10 19:13:45 +0100

    Used the dependencies

--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 const logger = require('morgan');
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
+server.use(helmet());
 server.use(logger('dev'));
 
 server.get('/', (req, res) =>
